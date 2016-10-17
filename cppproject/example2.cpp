d4928852b91dcdfce556f019306074e819243f8b
NeuroBind Project <axicsnetwork@gmail.com>
2016-10-17 15:34:38 +0800

    project 2

---
+++ cppproject/example2.cpp
@@ -0,0 +1,24 @@
+//Second project
+
+#include<iostream>
+using namespace std;
+
+void func_first();
+
+int main(){
+int = z;
+
+z = 20;
+func_first();
+cout << "\n";
+cout << z;
+
+return 0;
+}
+
+void func_first(){
+int z;
+
+z = 200;
+cout << z;
+}
